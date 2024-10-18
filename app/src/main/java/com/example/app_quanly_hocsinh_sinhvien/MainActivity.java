19374144066047f6f01687b1827ddae8628928e3
Anuts299 <letuananh7022020@gmail.com>
2024-10-18 17:45:31 +0700

    CardView Home P1

--- app/src/main/java/com/example/app_quanly_hocsinh_sinhvien/MainActivity.java
+++ app/src/main/java/com/example/app_quanly_hocsinh_sinhvien/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
@@ -26,7 +27,6 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- app/src/main/java/com/example/app_quanly_hocsinh_sinhvien/ui/HomeFragment.java
+++ app/src/main/java/com/example/app_quanly_hocsinh_sinhvien/ui/HomeFragment.java
@@ -2,7 +2,10 @@ package com.example.app_quanly_hocsinh_sinhvien.ui;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +15,31 @@ import com.example.app_quanly_hocsinh_sinhvien.R;
 
 
 public class HomeFragment extends Fragment {
-
+    private CardView class_card;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initUi(view);
+        initListener();
+        return view;
+    }
+    // Phương thức ánh xạ UI
+    private void initUi(View view) {
+        class_card = view.findViewById(R.id.class_card);
+    }
+
+    private void initListener() {
+        class_card.setOnClickListener(v -> {
+            // Chuyển sang ClassFragment
+            ClassFragment classFragment = new ClassFragment();
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, classFragment); // ID của container chứa Fragment
+            fragmentTransaction.addToBackStack(null); // Thêm vào back stack để có thể quay lại
+            fragmentTransaction.commit();
+        });
     }
 }
