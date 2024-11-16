3c1c7cda5968fa9922753bbdd478d01ce20d4584
Anuts299 <letuananh7022020@gmail.com>
2024-11-16 16:06:13 +0700

    Home

--- app/src/main/java/com/example/app_quanly_hocsinh_sinhvien/ui/HomeFragment.java
+++ app/src/main/java/com/example/app_quanly_hocsinh_sinhvien/ui/HomeFragment.java
@@ -33,7 +33,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home2, container, false);
         // Khởi tạo Firebase Firestore và FirebaseAuth
         db = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
