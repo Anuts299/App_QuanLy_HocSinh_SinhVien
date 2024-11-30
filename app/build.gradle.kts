a41dc79ea8bb9845c62054f4efe97122db431aec
Anuts299 <letuananh7022020@gmail.com>
2024-11-30 18:56:49 +0700

    Last

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,6 @@ dependencies {
     implementation ("com.google.android.material:material:1.9.0")
 
     implementation ("androidx.appcompat:appcompat:1.4.0") // Dùng phiên bản ổn định mới nhất
-    implementation ("com.google.android.material:material:1.5.0") // hoặc phiên bản mới nhất
 
     //Clans Fab github
     implementation ("com.github.clans:fab:1.6.4")
